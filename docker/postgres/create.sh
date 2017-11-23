0adb32eaa556ee2fd8428346950310add176d2c8
Aleksandr Barmin <barmin.alexander@gmail.com>
2017-11-23 21:35:00 +0300

    Docker updates

--- docker/postgres/create.sh
+++ docker/postgres/create.sh
@@ -1,1 +1,9 @@
-docker run --name mds_postgres -p 5432:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_USER=user -e POSTGRES_DB=appdb -d postgres:10.1 -v $(pwd)/pgdata:/var/lib/postgresql/data
+#!/bin/bash
+mkdir pgdata
+docker run --name mds_postgres \
+ -p 5432:5432 \
+ -e POSTGRES_PASSWORD=password \
+ -e POSTGRES_USER=user \
+ -e POSTGRES_DB=appdb \
+ -v $PWD/pgdata:/var/lib/postgresql/data \
+ -d postgres:10.1
