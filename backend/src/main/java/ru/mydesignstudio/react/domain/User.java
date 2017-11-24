80dfdc6ba4537193e79309725cbf42d7e5041507
Aleksandr Barmin <barmin.alexander@gmail.com>
2017-11-24 12:58:51 +0300

    User management works

--- backend/src/main/java/ru/mydesignstudio/react/domain/User.java
+++ backend/src/main/java/ru/mydesignstudio/react/domain/User.java
@@ -4,18 +4,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import ru.mydesignstudio.react.core.HaveDraft;
 
 @Entity
+@Table(name = "USERS")
 public class User implements HaveDraft {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 	@NotNull
-	@Size(min = 3)
     private String login = "";
     private String password = "";
     private boolean draft = true;
---
+++ backend/src/main/java/ru/mydesignstudio/react/service/user/UserRepository.java
@@ -0,0 +1,26 @@
+package ru.mydesignstudio.react.service.user;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ru.mydesignstudio.react.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+	/**
+	 * Find page of users by pageable object and with draft status.
+	 * @param pageInfo pageable info
+	 * @param draft draft status
+	 * @return page of users
+	 */
+	Page<User> findAllByDraft(Pageable pageInfo, boolean draft);
+	
+	/**
+	 * Find user by login.
+	 * @param login user's login
+	 * @return user
+	 */
+	User findOneByLogin(String login);
+}
